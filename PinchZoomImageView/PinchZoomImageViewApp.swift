43ec478425ce01bd9579a1ddac1ffaa4b8f3fe5b
사용자이름 <>
2023-01-24 11:24:58 +0900

    Initial Commit

---
+++ PinchZoomImageView/PinchZoomImageViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomImageViewApp.swift
+//  PinchZoomImageView
+//
+//  Created by Minyoung Yoo on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomImageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
